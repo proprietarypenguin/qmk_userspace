91b1c960260462afb570b26d53d5c1f2bbad7024
Jeffrey Abbott <jla.login@mailhaven.com>
2024-10-26 12:21:55 -0500

    adding other keyboards

---
+++ keyboards/crkbd/rules.mk
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+UNICODE_ENABLE = no         # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+RGB_MATRIX_ENABLE = no
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_DRIVER = WS2812
+LTO_ENABLE = yes
+
+# if firmware size over limit, try this option
+LTO_ENABLE = yes
+LAYOUTS = split_3x5_3 split_3x6_3
+
+# CFLAGS += -flto
+
+=======
+>>>>>>> upstream-updates
+DEFAULT_FOLDER = crkbd/rev1
